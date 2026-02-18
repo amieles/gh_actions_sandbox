8773999763bc69b5272be651e7b463a2edd288d4
amieles <alexmieles222@gmail.com>
2026-02-17 21:52:01 -0500

    test 6

--- sql_scripts/tables/table_script.sql
+++ sql_scripts/tables/table_script.sql
@@ -1,4 +1,4 @@
 create or replace table snowflake_sandbox_db.test_schema.test_table
 as
-select 5298
+select 529
 limit 200;
--- sql_scripts/views/view_script.sql
+++ sql_scripts/views/view_script.sql
@@ -1,4 +1,4 @@
 create or replace view snowflake_sandbox_db.test_schema.test_view
 as
 select {dt_lag}
-limit 52;
+limit 2;
