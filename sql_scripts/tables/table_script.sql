84186908b6b0be9c7ae0279dccfabcc52639d211
amieles <alexmieles222@gmail.com>
2026-02-18 21:39:04 -0500

    prayers up

--- sql_scripts/tables/table_script.sql
+++ sql_scripts/tables/table_script.sql
@@ -2,6 +2,6 @@ create or replace table snowflake_sandbox_db.test_schema.test_table
 (col1)
 as
 select 212 as col1
-limit 200;
+limit 20;
 
 grant select on table snowflake_sandbox_db.test_schema.test_table to role astro_role;
--- sql_scripts/test_db_name/views/view2_script.sql
+++ sql_scripts/test_db_name/views/view2_script.sql
@@ -3,6 +3,6 @@ as
 select 
 	{dt_lag} as col1,
 	'test_string' as col2
-limit 2;
+limit 225;
 
 grant select on snowflake_sandbox_db.test_schema.view2 to role non_existent_role2; 
