1ae918dc9bc8f185da65d88f12324d273c51fa56
amieles <alexmieles222@gmail.com>
2026-02-19 14:40:28 -0500

    pr test

--- sql_scripts/tables/table_script.sql
+++ sql_scripts/tables/table_script.sql
@@ -1,7 +1,7 @@
 create or replace table snowflake_sandbox_db.test_schema.test_table
 (col1)
 as
-select 212 as col1
+select 21 as col1
 limit 20;
 
 grant select on table snowflake_sandbox_db.test_schema.test_table to role astro_role;
