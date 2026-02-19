6c82f6f8be7595acb827646279dabe3e833eb09f
amieles <alexmieles222@gmail.com>
2026-02-18 20:32:40 -0500

    testing

--- sql_scripts/test_db_name/views/view2_script.sql
+++ sql_scripts/test_db_name/views/view2_script.sql
@@ -3,6 +3,6 @@ as
 select 
 	{dt_lag} as col1,
 	'test_string' as col2
-limit 52;
+limit 2;
 
 grant select on snowflake_sandbox_db.test_schema.view2 to role astro_role; 
--- sql_scripts/views/view_script.sql
+++ sql_scripts/views/view_script.sql
@@ -1,4 +1,4 @@
 create or replace view snowflake_sandbox_db.test_schema.test_view
 as
 select {dt_lag} as col1
-limit 52;
+limit 522;
